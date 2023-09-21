19788203187628aee462ebb6a16a6d9e593093a6
fuziji <142558519+fuziji@users.noreply.github.com>
2023-09-21 14:02:51 +0800

    Add files via upload

--- autoloading-deployment/autoloading-backend/autoloading/handlers/modbus.py
+++ autoloading-deployment/autoloading-backend/autoloading/handlers/modbus.py
@@ -1,7 +1,7 @@
 import socket,time
-from flask_socketio import SocketIO
 import sqlite3
-from datetime import datetime
+import threading
+from flask import Flask
 
 conn = sqlite3.connect('database.db')
 cursor = conn.cursor()
@@ -13,14 +13,12 @@ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 running = False
 int_distance = 0
 
-def read_sensor_data():
-    pass
 
-#@app.route('/get')
 def read_per_second():
     global int_distance
     global current_time
-    while True:
+    global running
+    while running:
         s.connect(server_ip)
         s.sendall(byte_data)
         received_data = s.recv(1024)
@@ -30,7 +28,9 @@ def read_per_second():
         int_distance = int(hex_distance,16)
         #print('distance:', int_distance,'mm')
         s.close()
+        insert_data(int_distance,current_time)
         #return int_distance
+
 
 def insert_data(int_distance,current_time):#将测量值和时间存储在数据库中
     conn = sqlite3.connect('database.db')
@@ -40,4 +40,23 @@ def insert_data(int_distance,current_time):#将测量值和时间存储在数据
 ''',(int_distance,current_time))
     conn.commit()
     conn.close()
-    time.sleep(1)
+
+
+def my_threading():
+    timer = threading.Timer(1,read_per_second,insert_data)
+    timer.start()
+
+
+@app.route('/start')
+def start():
+    global running
+    if not running:
+        running = True
+        threading.Thread(target=read_per_second).start()
+    return '测量开始'
+
+@app.route('/stop')
+def stop():
+    global running
+    running = False
+    return '测量停止'
